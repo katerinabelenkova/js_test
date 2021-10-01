ff4887c1893d3283aea8fa07f35c2bfde8b6cff5
katerinabelenkova <belenkova1003@gmail.com>
2021-10-02 00:08:22 +0300

    commit

---
+++ test3.js
@@ -0,0 +1,53 @@
+// let day = 25;
+// const isFirstDecade = (day >= 1 && day <=9);
+// const isSecondDecade = (day >= 10 && day <=19);
+// const isThirdDecade = (day >= 20 && day <=29);
+// const isFourthDecade = (day === 30 || day ===31);
+//
+// switch (true) {
+//     case isFirstDecade:
+//         console.log('FIRST DECADE');
+//     break;
+//     case isSecondDecade:
+//         console.log('SECOND DECADE');
+//         break;
+//     case isThirdDecade:
+//         console.log('THIRD DECADE');
+//         break;
+//     case isFourthDecade:
+//         console.log('FOURTH DECADE');
+//         break;
+//     default:
+//         console.log('INVALID');
+// }
+//
+// let num = 7;
+// let isSimple = true;
+//
+// for (let i = 2; i <=num -1; i++) {
+//     if (num % i===0){
+//        isSimple = false;
+//        break;
+//     }
+// }
+
+//console.log(isSimple ? 'простое' : 'сложное' );
+
+// function test(num) {
+//     for (let i = 2; i <= num - 1; i++){
+//         if (num % i ===0){
+//             return;
+//         }
+//     }
+//     return true;
+// }
+//
+// for (let i = 2; i <=10; i++){
+//     const isSimple = test(i);
+//
+//     if (isSimple) {
+//         console.log(i)
+//     }
+// }
+
+
